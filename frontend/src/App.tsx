f7ae749b85d71d5282395019e82ded97ed7b0e28
frans <franswinata6@gmail.com>
2024-02-05 11:53:41 +0700

    nambah routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,60 @@
-import { useState } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Menu from './components/Menu';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Layout = () => {
+    return (
+      <div className="w-full">
+        <Navbar />
+        <div>
+          <div>
+            <Menu />
+          </div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
 
-  return (
-    <></>
-  )
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/users',
+          element: <Users />,
+        },
+        {
+          path: '/products',
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: 'login',
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
